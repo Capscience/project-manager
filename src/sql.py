3c96c8f067402f5be29e487476b6095de6788417
Simo Soini <simo@capscience.fi>
2022-05-27 16:59:15 +0300

    Added all planned database tables to sql.py

--- src/sql.py
+++ src/sql.py
@@ -6,4 +6,35 @@ class Users(db.Model):
     name = db.Column(db.String(32), primary_key = True)
     password = db.Column(db.String(128))
 
+
+class Companies(db.Model):
+    """Companies that can be used as clients for projects."""
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String(32), unique = True)
+
+
+class Entries(db.Model):
+    """Project time entries."""
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id',
+                                                     ondelete = 'CASCADE'))
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+    comment = db.Column(db.String(128))
+
+
+class Projects(db.Model):
+    """Project data for users."""
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    state = db.Column(db.Integer)
+    name = db.Column(db.String(128))
+    user = db.Column(db.String(32), db.ForeignKey('users.name'),
+                                                  ondelete = 'CASCADE')
+    company_id = db.Column(db.Integer, db.ForeignKey('companies.id',
+                                                     ondelete = 'CASCADE'))
+
+
 db.create_all()
