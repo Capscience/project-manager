441d6e1426704caff03bdbe66346dad6aa382424
Simo Soini <simo@capscience.fi>
2022-05-31 21:34:08 +0300

    Started creating user interface for main functionality

--- src/user.py
+++ src/user.py
@@ -4,8 +4,8 @@ from flask import redirect
 from flask import url_for
 from src.app import app
 
-@app.route('/<usr>')
+@app.route('/user/<usr>')
 def user(usr):
     if not g.user:
         return redirect(url_for('home'))
-    return render_template('user.html')
+    return render_template('manager.html')
