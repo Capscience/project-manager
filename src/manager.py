2155e29b9067fdec6a0f5d0ebc0125ab7b2a8ac6
Simo Soini <simo@capscience.fi>
2022-06-02 21:01:50 +0300

    Added company creation and saving to database

--- src/manager.py
+++ src/manager.py
@@ -1,8 +1,36 @@
 from flask import render_template
-from src import app
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import request
+from flask import url_for
+from flask import flash
+from src import app, db
 from src.login import require_login
+from src import sql
 
-@app.route('/app')
+@app.route('/app', methods = ['GET', 'POST'])
 @require_login()
 def manager():
-    return render_template('manager.html')
+    """Handles the main app function."""
+
+    name = request.values.get('name')
+    if name:
+        if validate_company_name(name):
+            db.session.add(sql.Company(name = name))
+            db.session.commit()
+    companies = sql.Company.query.all()
+    return render_template('manager.html', companies = companies)
+
+def validate_company_name(name: str):
+    """Validate company name and add to database."""
+
+    if len(name) < 4 or name.count(' ') > 1:
+        flash('Company name should be at least 4 characters and contain no more than 1 space')
+        return False
+    db_name = sql.Company.query.filter_by(name = name).one_or_none()
+    if db_name == name:
+        flash(f'There already exists a compnay with name {name}.')
+        return False
+    return True
+            
