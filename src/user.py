1844719f7f95db60bbdcfc975da8a91ab04eb256
Simo Soini <simo@capscience.fi>
2022-07-01 16:52:48 +0300

    Added user deletion.

--- src/user.py
+++ src/user.py
@@ -1,13 +1,29 @@
 """App homepage."""
 
 from flask import render_template
+from flask import abort
+from flask import g
+from flask import request
+from flask import url_for
+from flask import redirect
+from flask import flash
 from src import app
+from src import db
 from src.login import require_login
 
 
-@app.route('/user/<uid>')
+@app.route('/user/<uid>', methods=['GET', 'POST'])
 @require_login()
-def user(uid: int):
-    """Render homepage."""
+def user(uid: str):
+    """Render user page and handle user deletion."""
 
+    if int(uid) != g.user[0]:
+        abort(403)
+
+    if request.values.get('action') == 'delete':
+        delete_sql = 'DELETE FROM account WHERE id=:uid'
+        db.session.execute(delete_sql, {'uid': g.user[0]})
+        db.session.commit()
+        flash('User deleted successfully.')
+        return redirect(url_for('logout'))
     return render_template('user.html')
